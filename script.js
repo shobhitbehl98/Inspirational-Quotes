6b0bea55076e3ff20f351450c5b9a7eecb4ff223
Shobhit Behl <shobhitbehl98@gmail.com>
2021-12-15 01:28:26 +0530

    Inspirational Quotes

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function randomIntFromInterval(min, max) {  
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  fetch('https://type.fit/api/quotes').then(data=>data.json()).then(Quote=>{
+    const t = randomIntFromInterval(0, Quote.length-1)
+    const q=Quote[t].text;
+    const a=Quote[t].author;
+    const d=document.getElementById('w')
+    d.innerHTML=q+"\n"+"-"+a;
+})
